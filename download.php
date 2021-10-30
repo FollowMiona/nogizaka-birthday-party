6fd88e9d7506a0b43997e07070a7cea7960e3756
FollowMiona <93431345+FollowMiona@users.noreply.github.com>
2021-10-30 13:33:32 +0200

    Add files via upload

---
+++ download.php
@@ -0,0 +1,119 @@
+<?php
+
+class ics_events
+{
+	const DT_FORMAT = 'Ymd\THis'; //define date format here
+	public $events;
+
+	function ics_events($events)
+	{
+		
+		if(count($events)>0)	
+		{
+			for($p=0;$p<=count($events)-1;$p++)
+			{
+				//echo '<br>p is:'.$p;
+				foreach($events[$p] as $key => $val) 
+				{
+				  $events[$p][$key] = $this->sanitize_val($val, $key);
+				}			
+			}
+		}
+		$this->events=$events;
+		
+	}
+
+	function prepare()
+	{
+		$cp=array();
+		if(count($this->events)>0)	
+		{
+		$cp[]= 'BEGIN:VCALENDAR';
+		$cp[]= 'VERSION:2.0';
+		$cp[]= 'PRODID:-//hacksw/handcal//NONSGML v1.0//EN';
+		$cp[]= 'CALSCALE:GREGORIAN';
+	
+			for($p=0;$p<=count($this->events)-1;$p++)
+			{
+				$cp[]='BEGIN:VEVENT';
+				foreach($this->events[$p] as $key => $val) 
+				{
+				  
+				  $cp[]= strtoupper($key).':'.$val;
+				 
+				}
+				$cp[]= 'RRULE:FREQ=YEARLY';
+				$cp[]= 'END:VEVENT';			
+			}
+			$cp[]='END:VCALENDAR';
+	
+		}
+		
+	//	echo "<pre>";
+	//	print_r($cp);
+		
+		return implode("\r\n", $cp);
+	
+	}
+	
+	private function sanitize_val($val, $key = false) 
+	{
+		switch($key) 
+		{
+	  		case 'dtend':
+	        case 'dtstamp':
+	  		case 'dtstart':
+		        $val = $this->format_timestamp($val);
+	    		break;
+	      default:
+		    $val = $this->escape_string($val);
+	    }
+		return $val;
+	}
+	private function format_timestamp($timestamp)
+	{
+		$dt = new DateTime($timestamp);
+		return $dt->format(self::DT_FORMAT);
+	}
+
+	private function escape_string($str)
+	{
+		return preg_replace('/([,;])/','\$1', $str);
+	}
+
+}
+
+//header('Content-Type: text/calendar; charset=utf-8');
+//header('Content-Disposition: attachment; filename=nogizaka46.ics');
+
+$events = array();
+
+$bdays = $_POST['bday'];
+
+//print_r($bdays);
+
+foreach($bdays as $k => $b){
+	//print_r($k);
+	//print_r($b);
+	$events[]=array(
+		'description' => '🎉 '.$k,
+		'summary' => '🎉 '.$k,
+		'dtstart' => $b,
+	);
+}
+
+
+$ics = new ics_events($events);
+
+$file = 'generated/nogizaka46_'.uniqid().'.ics';
+$current = file_get_contents($file);
+$current .= $ics->prepare();
+file_put_contents($file, $current);
+
+unset($ics);
+
+echo $file;
+die();
+
+
+?>
---
+++ index.php
@@ -0,0 +1,235 @@
+<!DOCTYPE html>
+
+<html dir="ltr" lang="nl-NL">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<meta charset="utf-8" />
+<meta name="apple-mobile-web-app-capable" content="yes">
+<meta name="description" content="Add Nogizaka46 members Birthdays to your calendar">
+<meta name="author" content="Miona">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Nogizaka46 Birthday Party! </title>
+<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+<link href="https://fonts.googleapis.com/css2?family=Lora:wght@400;600&display=swap" rel="stylesheet">
+<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
+
+</head>
+<body>
+
+<div class="container mt-3">
+	<div class="row">
+		<div class="col-12 center">
+			<h1>Nogizaka46 Birthday Party <sup><small>beta</small></sup></h1>
+			<p>1. Select the members of your liking.<br>
+				2. Press the 'generate' button to create the file.<br>
+				3. Download the file to add the Birthdays to your calendar.</p>
+
+				<p>The Birthdays are recurring, starting from the members birth year.</p>
+
+
+
+		</div>
+	</div>
+	<div class="row nogizakadaisuki">
+		<div class="col-12">
+			<h2>1st gen</h2><span class="btn button checkall" data-gen="gen1">Select all 1st gen members</span>
+		</div>
+	</div>
+	<div class="row" id="gen1">
+		<div class="col-12 col-md-6">
+			<span><input type="checkbox" data-name="Akimoto Manatsu" data-date="1993-08-20"> Akimoto Manatsu - August 20</span><br>
+			<span><input type="checkbox" data-name="Ikuta Erika" data-date="1997-01-22"> Ikuta Erika - January 22</span><br>
+			<span><input type="checkbox" data-name="Higuchi Hina" data-date="1998-01-31"> Higuchi Hina - January 31</span><br>
+			<span><input type="checkbox" data-name="Hoshino Minami" data-date="1998-02-06"> Hoshino Minami - February 6</span><br>
+		</div>
+		<div class="col-12 col-md-6">
+			<span><input type="checkbox" data-name="Saito Asuka" data-date="1998-08-10"> Saito Asuka - August 10</span><br>
+			<span><input type="checkbox" data-name="Takayama Kazumi" data-date="1994-02-08"> Takayama Kazumi - February 8</span><br>
+			<span><input type="checkbox" data-name="Wada Maaya" data-date="1998-04-23"> Wada Maaya - April 23</span><br>
+		</div>
+	</div>
+
+
+	<div class="row nogizakadaisuki">
+		<div class="col-12">
+			<h2>2nd gen</h2><span class="btn button checkall" data-gen="gen2">Select all 2nd gen members</span>
+		</div>
+	</div>
+	<div class="row" id="gen2">
+		<div class="col-12 col-md-6">
+			<span><input type="checkbox" data-name="Kitano Hinako" data-date="1996-07-17"> Kitano Hinako - July 17</span><br>
+			<span><input type="checkbox" data-name="Shinuchi Mai" data-date="1992-01-22"> Shinuchi Mai - January 22</span><br>
+			<span><input type="checkbox" data-name="Suzuki Ayane" data-date="1999-03-05"> Suzuki Ayane - March 5</span><br>
+		</div>
+		<div class="col-12 col-md-6">
+			<span><input type="checkbox" data-name="Terada Ranze" data-date="1998-09-23"> Terada Ranze - September 23</span><br>
+			<span><input type="checkbox" data-name="Yamazaki Rena" data-date="1997-05-21"> Yamazaki Rena - May 21</span><br>
+		</div>
+	</div>
+
+
+	<div class="row nogizakadaisuki">
+		<div class="col-12">
+			<h2>3rd gen</h2><span class="btn button checkall" data-gen="gen3">Select all 3rd gen members</span>
+		</div>
+	</div>
+	<div class="row" id="gen3">
+		<div class="col-12 col-md-6">
+			<span><input type="checkbox" data-name="Ito Riria" data-date="2002-10-08"> Ito Riria - October 8</span><br>
+			<span><input type="checkbox" data-name="Iwamoto Renka" data-date="2004-02-02"> Iwamoto Renka - February 2</span><br>
+			<span><input type="checkbox" data-name="Kubo Shiori" data-date="2001-07-14"> Kubo Shiori - July 14</span><br>
+			<span><input type="checkbox" data-name="Mukai Hazuki" data-date="1999-08-23"> Mukai Hazuki - August 23</span><br>
+			<span><input type="checkbox" data-name="Nakamura Reno" data-date="2001-09-27"> Nakamura Reno - September 27</span><br>
+
+		</div>
+		<div class="col-12 col-md-6">
+			<span><input type="checkbox" data-name="Sakaguchi Tamami" data-date="2001-11-10"> Sakaguchi Tamami - November 10</span><br>
+			<span><input type="checkbox" data-name="Satou Kaede" data-date="1998-03-23"> Satou Kaede - March 23</span><br>
+			<span><input type="checkbox" data-name="Umezawa Minami" data-date="1999-01-06"> Umezawa Minami - January 6</span><br>
+			<span><input type="checkbox" data-name="Yamashita Mizuki" data-date="1999-07-26"> Yamashita Mizuki - July 26</span><br>
+			<span><input type="checkbox" data-name="Yoda Yuuki" data-date="2000-05-05"> Yoda Yuuki - May 5</span><br>
+			<span><input type="checkbox" data-name="Yoshida Ayano Christie" data-date="1995-09-06"> Yoshida Ayano Christie - September 6</span><br>
+		</div>
+	</div>
+
+	<div class="row nogizakadaisuki">
+		<div class="col-12">
+			<h2>4th gen</h2><span class="btn button checkall" data-gen="gen4">Select all 4th gen members</span>
+		</div>
+	</div>
+	<div class="row" id="gen4">
+		<div class="col-12 col-md-6">
+			<span><input type="checkbox" data-name="Endou Sakura" data-date="2001-10-03"> Endou Sakura - October 3</span><br>
+			<span><input type="checkbox" data-name="Hayakawa Seira" data-date="2000-08-24"> Hayakawa Seira - August 24</span><br>
+			<span><input type="checkbox" data-name="Hayashi Runa" data-date="2003-10-02"> Hayashi Runa - October 2</span><br>
+			<span><input type="checkbox" data-name="Kakehashi Sayaka" data-date="2002-11-20"> Kakehashi Sayaka - November 20</span><br>
+			<span><input type="checkbox" data-name="Kaki Haruka" data-date="2001-08-08"> Kaki Haruka - August 8</span><br>
+			<span><input type="checkbox" data-name="Kanagawa Saya" data-date="2001-10-31"> Kanagawa Saya - October 31</span><br>
+			<span><input type="checkbox" data-name="Kitagawa Yuri" data-date="2001-08-08"> Kitagawa Yuri - August 8</span><br>
+			<span><input type="checkbox" data-name="Kuromi Haruka" data-date="2004-01-19"> Kuromi Haruka - January 19</span><br>
+		</div>
+		<div class="col-12 col-md-6">
+			<span><input type="checkbox" data-name="Matsuo Miyu" data-date="2004-01-03"> Matsuo Miyu - January 3</span><br>
+			<span><input type="checkbox" data-name="Satou Rika" data-date="2001-08-09"> Satou Rika - August 9</span><br>
+			<span><input type="checkbox" data-name="Seimiya Rei" data-date="2003-08-01"> Seimiya Rei - August 1</span><br>
+			<span><input type="checkbox" data-name="Shibata Yuna" data-date="2003-03-03"> Shibata Yuna - March 3</span><br>
+			<span><input type="checkbox" data-name="Tamura Mayu" data-date="1999-01-12"> Tamura Mayu - January 12</span><br>
+			<span><input type="checkbox" data-name="Tsutsui Ayame" data-date="2004-06-08"> Tsutsui Ayame - June 8</span><br>
+			<span><input type="checkbox" data-name="Yakubo Mio" data-date="2002-08-14"> Yakubo Mio - August 14</span><br>
+			<span><input type="checkbox" data-name="Yumiki Nao" data-date="1999-02-03"> Yumiki Nao - February 3</span><br>
+		</div>
+	</div>
+
+	<div class="row nogizakadaisuki">
+		<div class="col-12">
+			<h2>Graduated</h2><span class="btn button checkall" data-gen="grad">Select all graduated members</span>
+		</div>
+	</div>
+	<div class="row" id="grad">
+		<div class="col-12 col-md-6">
+			<span><input type="checkbox" data-name="Andou Mikumo" data-date="1993-05-21"> Andou Mikumo - May 21</span><br>
+			<span><input type="checkbox" data-name="Eto Misa" data-date="1993-01-04"> Eto Misa - January 4</span><br>
+			<span><input type="checkbox" data-name="Fukagawa Mai" data-date="1991-03-29"> Fukagawa Mai - March 29</span><br>
+			<span><input type="checkbox" data-name="Hashimoto Nanami" data-date="1993-02-20"> Hashimoto Nanami - February 20</span><br>
+			<span><input type="checkbox" data-name="Hatanaka Seira" data-date="1995-12-05"> Hatanaka Seira - December 5</span><br>
+			<span><input type="checkbox" data-name="Hori Miona" data-date="1996-10-15"> Hori Miona - October 15</span><br>
+			<span><input type="checkbox" data-name="Ichiki Rena" data-date="1996-01-22"> Ichiki Rena - January 22</span><br>
+			<span><input type="checkbox" data-name="Ikoma Rina" data-date="1995-12-29"> Ikoma Rina - December 29</span><br>
+			<span><input type="checkbox" data-name="Inoue Sayuri" data-date="1994-12-14"> Inoue Sayuri - December 14</span><br>
+			<span><input type="checkbox" data-name="Ito Junna" data-date="1998-11-30"> Ito Junna - November 30</span><br>
+			<span><input type="checkbox" data-name="Ito Karin" data-date="1993-05-26"> Ito Karin - May 26</span><br>
+			<span><input type="checkbox" data-name="Ito Marika" data-date="1996-02-20"> Ito Marika - February 20</span><br>
+			<span><input type="checkbox" data-name="Ito Nene" data-date="1995-12-12"> Ito Nene - December 12</span><br>
+			<span><input type="checkbox" data-name="Iwase Yumiko" data-date="1990-06-12"> Iwase Yumiko - June 12</span><br>
+			<span><input type="checkbox" data-name="Kashiwa Yukina" data-date="1994-08-12"> Kashiwa Yukina - August 12</span><br>
+			<span><input type="checkbox" data-name="Kawago Hina" data-date="1998-03-22"> Kawago Hina - March 22</span><br>
+			<span><input type="checkbox" data-name="Kawamura Mahiro" data-date="1995-07-23"> Kawamura Mahiro - July 23</span><br>
+			<span><input type="checkbox" data-name="Matsumura Sayuri" data-date="1992-08-27"> Matsumura Sayuri - August 27</span><br>
+			<span><input type="checkbox" data-name="Miyazawa Seira" data-date="1993-10-29"> Miyazawa Seira - October 29</span><br>
+			<span><input type="checkbox" data-name="Nagashima Seira" data-date="1994-05-19"> Nagashima Seira - May 19</span><br>
+		</div>
+		<div class="col-12 col-md-6">
+			<span><input type="checkbox" data-name="Nakada Kana" data-date="1994-08-06"> Nakada Kana - August 6</span><br>
+			<span><input type="checkbox" data-name="Nakamoto Himeka" data-date="1996-04-13"> Nakamoto Himeka - April 13</span><br>
+			<span><input type="checkbox" data-name="Nishikawa Nanami" data-date="1993-07-03"> Nishikawa Nanami - July 3</span><br>
+			<span><input type="checkbox" data-name="Nishino Nanase" data-date="1994-03-24"> Nishino Nanase - May 24</span><br>
+			<span><input type="checkbox" data-name="Noujou Ami" data-date="1994-10-18"> Noujou Ami - October 18</span><br>
+			<span><input type="checkbox" data-name="Ozono Momoko" data-date="1999-09-13"> Ozono Momoko - September 13</span><br>
+			<span><input type="checkbox" data-name="Sagara Iori" data-date="1997-11-26"> Sagara Iori - November 26</span><br>
+			<span><input type="checkbox" data-name="Saito Chiharu" data-date="1997-02-17"> Saito Chiharu - February 17</span><br>
+			<span><input type="checkbox" data-name="Saito Yuuri" data-date="1993-07-20"> Saito Yuuri - July 20</span><br>
+			<span><input type="checkbox" data-name="Sakurai Reika" data-date="1994-05-16"> Sakurai Reika - May 16</span><br>
+			<span><input type="checkbox" data-name="Sasaki Kotoko" data-date="1998-08-28"> Sasaki Kotoko - August 28</span><br>
+			<span><input type="checkbox" data-name="Shiraishi Mai" data-date="1992-08-20"> Shiraishi Mai - August 20</span><br>
+			<span><input type="checkbox" data-name="Wakatsuki Yumi" data-date="1994-06-27"> Wakatsuki Yumi - June 27</span><br>
+			<span><input type="checkbox" data-name="Watanabe Miria" data-date="1999-11-01"> Watanabe Miria - November 1</span><br>
+			<span><input type="checkbox" data-name="Yada Risako" data-date="1995-03-08"> Yada Risako - March 8</span><br>
+			<span><input type="checkbox" data-name="Yamamoto Honoka" data-date="1998-03-31"> Yamamoto Honoka - March 31</span><br>
+			<span><input type="checkbox" data-name="Yamato Rina" data-date="1994-12-14"> Yamato Rina - December 14</span><br>
+			<span><input type="checkbox" data-name="Yonetoku Kyoka" data-date="1999-04-14"> Yonetoku Kyoka - April 14</span><br>
+			<span><input type="checkbox" data-name="Yoshimoto Ayaka" data-date="1996-08-18"> Yoshimoto Ayaka - August 18</span><br>
+		</div>
+	</div>
+
+	<div class="row">
+		<div class="col-12 center">
+			<div class="generate">
+				<span class="btn button generatebutton" id="generate">Create link</span>
+				<span class="downloadlink"></span>
+			</div>
+
+		</div>
+	</div>
+</div>
+
+<footer class="mt-3 mb-3">
+	<p class="text-center center"><small>Please report bugs or ideas to <a target="_blank" href="https://twitter.com/FollowMiona">Miona</a></small></p>
+</footer>
+</body>
+
+<script>
+	jQuery(document).ready(function($){
+
+	//	bdayObject = [];
+
+		$("#generate").on("click", function(){
+
+			bdayObject = {};
+
+			$('input[type=checkbox]').each(function () {
+	           if (this.checked) {
+	               bdayObject[$(this).data("name")] = $(this).data("date");
+	           }
+			});
+
+			if (Object.keys(bdayObject).length === 0) {
+				$(".downloadlink").html('<span class="uhoh">Please select at least one member.</span>');
+				return false;
+			}
+
+			$.ajax('download.php', {
+			    type: 'POST',
+			    dataTyp: 'json',
+			    data: {
+			    	bday: bdayObject
+			    },
+			    success: function (data, status, xhr) {
+			       $(".downloadlink").html('<a href="'+data+'">Calendar generated, click here to download</a>');
+			    },
+			    error: function (jqXhr, textStatus, errorMessage) {
+		           console.log(errorMessage);
+			    }
+			});
+		});
+
+		$(".checkall").on("click", function(){
+			gen = $(this).data("gen");
+
+			$("#"+gen).find("input").prop("checked", true);
+		});
+
+	});
+</script>
